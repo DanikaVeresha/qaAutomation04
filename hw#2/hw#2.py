10e3bd996528a1b9bbf6c7748b9fe8b3cb767ef9
desh288ddiesh <deshdesh288@gmail.com>
2024-04-16 00:53:41 +0300

    hw#2#3#4

---
+++ hw#2/hw#2.py
@@ -0,0 +1,4 @@
+
+item_list = "Murzik, Barsik, Pantera"
+item_list_corteg = ('Murzik', 'Barsik', 'Pantera')
+print(f'List: {item_list}')
---
+++ hw#4/hw#4.py
@@ -0,0 +1,8 @@
+
+x = int(input('Enter your value "x":'))
+y = int(input('Enter your value "y":'))
+res_sum = x + y
+res_product = x * y
+info = [res_sum, res_product]
+for item in info:
+    print(item)
