8076f8fe3c2fb08d8c0b8b635012b0f06b7dfb54
desh288ddiesh <deshdesh288@gmail.com>
2024-04-19 17:20:25 +0300

    hw#5#6#7#8#9

--- hw#9/hw#9.py
+++ hw#9/hw#9.py
@@ -1,16 +1,11 @@
 
 number = int(input('Enter value: '))
 
-if number >= 1 and number <= 9:
-    k = (2 * number) - 2
-    for i in range(0, number):
-        for j in range(0, k):
-            print(end=" ")
-        k = k - 1
-        for j in range(0, i + 1):
-            print(number, end=' ')
-        print(" ")
-else:
-    print('Error: Your values "n" invalid\n'
-          'Please enter valid values "n" where 1 <= n <= 9')
-
+m = (2 * number) - 2
+for i in range(0, number):
+    for j in range(0, m):
+        print(end=" ")
+    m = m - 1
+    for j in range(0, i + 1):
+        print(i, end=' ')
+    print(" ")
