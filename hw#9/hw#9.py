d3d307c06a0f465d6bf4ee20c6e5aec8c406cf53
desh288ddiesh <deshdesh288@gmail.com>
2024-04-21 18:27:06 +0300

    hw#5#6#7#8#9

--- hw#9/hw#9.py
+++ hw#9/hw#9.py
@@ -1,10 +1,10 @@
 
 
-n = int(input("Enter value 'n': "))
+n = int(input("Enter a number: "))
 for j in range(1, n+1):
-    print(' ' * 2 * (n-j), end='')
-    for i in range(1, 2 * j):
-        print(i if i <= j else 2 * j-i, end='')
-        if i < 2 * j - 1:
+    print(' '*(3*(n-j)), end='')
+    for i in range(1, 2*j):
+        print(f'{i:2}' if i <=j else f'{2*j-i:2}', end='')
+        if i<2*j-1:
             print(' ', end='')
     print()
