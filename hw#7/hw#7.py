7fbd4e96009f4f96ba371d1cc6d890c4556de13b
desh288ddiesh <deshdesh288@gmail.com>
2024-04-23 00:59:40 +0300

    hw#5#6#7#8#9

--- hw#7/hw#7.py
+++ hw#7/hw#7.py
@@ -1,8 +1,8 @@
 
 triangle = int(input('Enter size of triangle: '))
+symbol = '*'
 
 for item in range(triangle):
-    symbol = '*'
     for j in range(triangle-1, -1, -1):
         if j > item:
             print(" ", end="")
