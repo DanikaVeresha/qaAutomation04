e0a3daf3f8a5323861bf572d04b1023adb95003a
desh288ddiesh <deshdesh288@gmail.com>
2024-04-25 20:56:39 +0300

    hw#11

--- hw#11/hw#11.py
+++ hw#11/hw#11.py
@@ -1,10 +1,11 @@
+
 sentences = "Hello all. Here`s pretty cold and hot. Choose yourself"
 arr = sentences.split('.')
-print(arr)
+print(f'Sentences: {arr}')
 result = []
 for i in arr:
     result += str(len(i.split()))
-print(f'[{", ".join(result)}]')
+print(f'Result: [{", ".join(result)}]')
 
 
 
