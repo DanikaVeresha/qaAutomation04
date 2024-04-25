3acf8c9dd68b60ae07bbca488611a131cafb2c05
desh288ddiesh <deshdesh288@gmail.com>
2024-04-25 15:40:11 +0300

    hw#11

---
+++ hw#11/hw#11.py
@@ -0,0 +1,21 @@
+
+sentences = "Hello all. Here`s pretty cold and hot. Choose yourself"
+arr = sentences.split('.')
+print(arr)
+result = ''
+for i in arr:
+    result += str(len(i.split())) + ' '
+print(result)
+print(type(result))
+
+
+
+
+
+
+
+
+
+
+
+
