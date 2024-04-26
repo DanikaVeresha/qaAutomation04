5c7e0774c4894177e6bbee960886ec5e5c335596
desh288ddiesh <deshdesh288@gmail.com>
2024-04-26 19:15:16 +0300

    hw#13

--- hw#13/hw#13.py
+++ hw#13/hw#13.py
@@ -1,24 +1,23 @@
 
 email = 'Desh288diesh@gmail.com'
-# email = '@desh288@gmail.com'
-# email = 'desh288@gmail.com.'
-# email = 'desh288@gmailcom'
-# email = 'desh288gmail.com'
-# email = 'desh288@gmail@.com'
-# email = 'desh288@gmail..com'
+# email = 'Desh288diesh.gmail@com'
+# email = '@desh288dieshgmail.com'
+# email = 'desh288diesh@gmailcom.'
+# email = 'desh_288diesh@gmail.com'
+# email = 'desh288diesh@gmail.co_m'
+# email = 'desh288diesh@@gmail.com'
+# email = 'desh288@diesh@gmail.c.om'
 
+check_email_1 = email.find('@') < email.find('.')
+check_email_2 = email.find('@') != 0 and email.find('.') != len(email) - 1
+check_email_3 = email.split("@")[0].isalnum() and email.split(".")[1].isalnum()
+check_email_4 = email.count('@') == 1 and email.count('.') == 1
 
-check_email = (email.find('@') < email.find('.') and
-               email.find('.') != -1 and email.find('@') != 0 and
-               (email.isalnum() or email.isascii()) and
-               email.count('@') == 1 and email.count('.') == 1)
-
-if check_email:
-    print(f'Email: {email} - is correct\n'
-          f'Result checking email: {check_email}')
+if check_email_1 and check_email_2 and check_email_3 and check_email_4:
+    print(f'Email: {email} - is correct')
 else:
-    print(f'Email: {email} - is incorrect\n'
-          f'Result checking email: {check_email}')
+    print(f'Email: {email} - is incorrect')
+
 
 
 
