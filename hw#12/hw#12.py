24885c3910abbe2060cdf475ffd150c80523cb1a
desh288ddiesh <deshdesh288@gmail.com>
2024-04-26 17:08:16 +0300

    hw#12

--- hw#12/hw#12.py
+++ hw#12/hw#12.py
@@ -1,15 +1,15 @@
 
 string = "This tool is cool. But that owl is awful. MAGIC TOOLS Ltd."
+# string = "This toool is cool. But that owl is awful. MAGIC TOOOLS Ltd."
+
 print(f'String: {string}')
 symbol = "o"
 result = ''
-for item in string.split('.'):
+for item in string.lower().split('.'):
     for i in item.split():
-        if (i.count(symbol, 0, len(i)) > 1
-                or i.count(symbol.upper(), 0, len(i)) > 1):
+        if i.count(symbol) == 2:
             result += i.capitalize() + ' '
 print(f'Result: {". ".join(result.split())}')
-
 
 
 
