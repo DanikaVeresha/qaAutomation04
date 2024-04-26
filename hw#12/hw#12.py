4824e42a5112b3af5d59c1a3f913421676abc902
desh288ddiesh <deshdesh288@gmail.com>
2024-04-26 19:26:34 +0300

    hw#12

--- hw#12/hw#12.py
+++ hw#12/hw#12.py
@@ -11,6 +11,12 @@ for item in string.lower().split('.'):
             result += i.capitalize() + ' '
 print(f'Result: {". ".join(result.split())}')
 
+# symbol = "o"
+# result = []
+# for item in string.lower().split():
+#     if item.count(symbol) == 2:
+#         result.append(item)
+# print(f'Result: {" ".join(result).title()}')
 
 
 
