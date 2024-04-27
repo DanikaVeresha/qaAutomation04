57eb813f3c75fa8cddd6724eb977e8c0f605d9ef
desh288ddiesh <deshdesh288@gmail.com>
2024-04-27 04:15:09 +0300

    hw#13

--- hw#13/hw#13.py
+++ hw#13/hw#13.py
@@ -1,22 +1,65 @@
+# email = ' '
+email = 'aaa@bbb.ccc'
 
-email = 'Desh288diesh@gmail.com'
-# email = 'Desh288diesh.gmail@com'
-# email = '@desh288dieshgmail.com'
-# email = 'desh288diesh@gmailcom.'
-# email = 'desh_288diesh@gmail.com'
-# email = 'desh288diesh@gmail.co_m'
-# email = 'desh288@diesh@gmail.com'
-# email = 'desh288diesh@gmail.c.om'
+# email = '@aaabbbccc.'
+# email = '@aaabbbccc'
+# email = '.aaabbbccc'
+# email = 'aaabbbccc@'
+# email = 'aaabbbccc.'
 
-check_email_1 = email.find('@') < email.find('.')
-check_email_2 = email.find('@') != 0 and email.find('.') != len(email) - 1
-check_email_3 = email.split("@")[0].isalnum() and email.split(".")[1].isalnum()
-check_email_4 = email.count('@') == 1 and email.count('.') == 1
+# email = 'aaa@bbbccc'
+# email = 'aaabbb.ccc'
+# email = 'aaa@bb@b.ccc'
+# email = 'aaa@bbb.c.cc'
+# email = 'aaabbbccc'
 
-if check_email_1 and check_email_2 and check_email_3 and check_email_4:
-    print(f'Email: {email} - is correct')
-else:
-    print(f'Email: {email} - is incorrect')
+# email = 'a@aa@@bbbccc'
+# email = 'aaabbb...ccc'
+# email = 'a@aab@bb@.ccc'
+# email = 'aaa@bbb.c.c.c'
+
+# email = 'a_aa@bbb.cc/c'
+# email = '_aaa@bbb.ccc/'
+
+# email = 'aaa@bbb.c@c@c'
+# email = 'aaa.bbb.ccc@cc.c'
+
+condition_0 = (email != '') # True
+
+condition_1 = email.find('@') < email.find('.') # True
+condition_2 = email.find('.') > email.find('@') # False
+
+condition_3 = email.find('@') != 0 and email.find('.') != len(email) - 1 # True
+condition_4 = email.find('@') == 0 and email.find('.') == len(email) - 1 # False
+condition_5 = email.find('@') == 0 or email.find('.') == len(email) - 1 # False
+condition_6 = email.find('.') == 0 or email.find('@') == len(email) - 1 # False
+
+condition_7 = email.replace('@', '').replace('.', '').isalnum() # True
+
+condition_8 = email.count('@') == 1 and email.count('.') == 1 # True
+condition_9 = email.count('@') == 0 and email.count('.') == 0 # False
+condition_10 = email.count('.') > 1 and email.count('@') > 1 # False
+condition_11 = email.count('.') == 0 or email.count('@') >= 1 # False
+condition_12 = email.count('.') >= 1 or email.count('@') == 0 # False
+
+successful_check_email = (condition_0 and condition_1 and condition_3 and condition_7
+                          and condition_8)
+unsuccesful_check_email = (condition_2 or condition_4 or condition_5 or condition_6
+                           or condition_9 or condition_10 or condition_11 or condition_12)
+
+try:
+    if successful_check_email:
+        print(f'Email: {email} - is correct "True"')
+    elif unsuccesful_check_email:
+        print(f'Email: {email} - is incorrect "False"')
+    else:
+        print(f'Email: {email} - test case not covered')
+except ValueError as ve:
+    print(ve)
+
+
+
+
 
 
 
