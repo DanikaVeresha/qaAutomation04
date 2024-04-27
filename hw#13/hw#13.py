8a2a751a09f02f2ef5777c762a08de052c4b7eee
desh288ddiesh <deshdesh288@gmail.com>
2024-04-27 12:02:06 +0300

    hw#13

--- hw#13/hw#13.py
+++ hw#13/hw#13.py
@@ -1,63 +1,40 @@
-email = ' '
-# email = 'aaa@bbb.ccc'
+# email = ' '
+email = 'aaa277@bbb.ccc' # True
+# email = '@aaa277bbbccc.'
+# email = '@aaa277bbbccc'
+# email = '.aaa277bbbccc'
+# email = 'aaa277bbbccc@'
+# email = 'aaa277bbbccc.'
 
-# email = '@aaabbbccc.'
-# email = '@aaabbbccc'
-# email = '.aaabbbccc'
-# email = 'aaabbbccc@'
-# email = 'aaabbbccc.'
+# email = 'aaa277@bbbccc'
 
-# email = 'aaa@bbbccc'
-# email = 'aaabbb.ccc'
-# email = 'aaa@bb@b.ccc'
-# email = 'aaa@bbb.c.cc'
-# email = 'aaabbbccc'
-
-# email = 'a@aa@@bbbccc'
-# email = 'aaabbb...ccc'
-# email = 'a@aab@bb@.ccc'
-# email = 'aaa@bbb.c.c.c'
+# email = 'aaa277bbb.ccc'
+# email = 'aaa277@bbbccc'
+# email = 'aaa277@bbb.c.cc'
+# email = 'aaa@277@bbb.ccc'
 
 # email = 'a_aa@bbb.cc/c'
 # email = '_aaa@bbb.ccc/'
 
 # email = 'aaa@bbb..c@c@c'
 # email = 'aaa.bbb.ccc@c@c.c'
-# email = 'aaa.bbbccc'
-# email = 'aaa@bbbccc'
 
-condition_0 = (email != '') # PositiveTC
+check_email = (email != ' '
+               and (email.find('@') < email.find('.'))
+               and email.find('@') != 0
+               and email.find('.') != len(email) - 1
+               and (email.replace('@', '').replace('.', '').isalnum())
+               and email.count('@') == 1
+               and email.count('.') == 1
+               and email.count('@') != 0
+               and email.count('.') != 0)
 
-condition_1 = email.find('@') < email.find('.') # PositiveTC
-condition_2 = email.find('.') > email.find('@') # NegativeTC
+if check_email:
+    print(f'Result check email: {email} - "True"')
+else:
+    print(f'Result check email: {email} - "False"')
 
-condition_3 = email.find('@') != 0 and email.find('.') != len(email) - 1 # PositiveTC
-condition_4 = email.find('@') == 0 and email.find('.') == len(email) - 1 # NegativeTC
-condition_5 = email.find('@') == 0 or email.find('.') == len(email) - 1 # NegativeTC
-condition_6 = email.find('.') == 0 or email.find('@') == len(email) - 1 # NegativeTC
 
-condition_7 = email.replace('@', '').replace('.', '').isalnum() # PositiveTC
-
-condition_8 = email.count('@') == 1 and email.count('.') == 1 # PositiveTC
-condition_9 = email.count('@') == 0 and email.count('.') == 0 # NegativeTC
-condition_10 = email.count('.') > 1 and email.count('@') > 1 # NegativeTC
-condition_11 = email.count('.') == 0 or email.count('@') == 0 # NegativeTC
-condition_12 = email.count('.') >= 1 or email.count('@') >= 1 # NegativeTC
-
-correct_email = (condition_0 and condition_1 and condition_3 and condition_7
-                          and condition_8)
-incorrect_email = (condition_2 or condition_4 or condition_5 or condition_6
-                           or condition_9 or condition_10 or condition_11 or condition_12)
-
-try:
-    if correct_email:
-        print(f'Email: {email} - is correct "True"')
-    elif incorrect_email:
-        print(f'Email: {email} - is incorrect "False"')
-    else:
-        print(f'Email: {email} - test case not covered')
-except ValueError as ve:
-    print(ve)
 
 
 
