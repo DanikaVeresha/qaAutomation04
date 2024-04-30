a27711f528c8f5879592f2aad4956db6de1c1866
desh288ddiesh <deshdesh288@gmail.com>
2024-04-30 21:45:21 +0300

    hw#14

---
+++ lecture#5/hw#14.py
@@ -0,0 +1,11 @@
+number_list = range(16)
+print(list(number_list))    # List of numbers from 0 to 15
+
+# Elements are divisible by 3 but not by 5
+print(list(item for item in number_list if item % 3 == 0 and item % 5 != 0))
+
+# Elements are divisible by 5 but not by 3
+print(list(item for item in number_list if item % 5 == 0 and item % 3 != 0))
+
+# Elements are divisible by 3 and 5
+print(list(item for item in number_list if item % 3 == 0 and item % 5 == 0))
