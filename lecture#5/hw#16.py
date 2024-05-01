86f6a914dbea913f2cd5ce84d60fcba02cf4a898
desh288ddiesh <deshdesh288@gmail.com>
2024-05-01 17:12:55 +0300

    hw#16

--- lecture#5/hw#16.py
+++ lecture#5/hw#16.py
@@ -1,22 +1,12 @@
 # lst = [1, 2, 3, 4, 5]
-lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# lst = [10, 12, 30]
+lst = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5]
 
 # Run each option separately since there is only one list !!!!
-
-
-# Training option 1
-lst2 = lst.copy()
-result = [(i+(i + 1))/2 for i in lst2 if i != lst2[-1]]
+lst1 = lst.copy()
+result = [(lst1[i]+(lst1[i+1]))/2 for i in range(len(lst1)-1) if i != lst1[-1]]
 for item in result:
-    lst2.insert(result.index(item)*2+1, item)
-print(lst2)
-
-# Training option 2
-result = [(i+(i + 1))/2 for i in lst if i != lst[-1]]
-main_result = []
-for item in result:
-    main_result.append(lst[result.index(item)])
-    main_result.append(item)
-main_result.append(lst[-1])
-print(main_result)
+    lst1.insert(result.index(item)*2+1, item)
+print(lst1)
 
