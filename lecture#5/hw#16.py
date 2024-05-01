1d8932cb38a610b08061015a5d904442d458a72b
desh288ddiesh <deshdesh288@gmail.com>
2024-05-01 17:13:41 +0300

    hw#16

--- lecture#5/hw#16.py
+++ lecture#5/hw#16.py
@@ -3,7 +3,7 @@
 # lst = [10, 12, 30]
 lst = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5]
 
-# Run each option separately since there is only one list !!!!
+# Method 1
 lst1 = lst.copy()
 result = [(lst1[i]+(lst1[i+1]))/2 for i in range(len(lst1)-1) if i != lst1[-1]]
 for item in result:
