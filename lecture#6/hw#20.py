f7502368418ab95005cc42028b6c5049ed0bdaac
desh288ddiesh <deshdesh288@gmail.com>
2024-05-05 15:42:59 +0300

    hw#20

--- lecture#6/hw#20.py
+++ lecture#6/hw#20.py
@@ -1,20 +1,13 @@
 
-# with open('hw#20.txt', 'x') as f:
-# Спочатку я виконала такий варіант, для створення даного файлу та запису в нього нищевказаних даних!
 
 with open('hw#20.txt', 'w') as f:
-    f.writelines(['Hello\n', 'Hello World\n', 'desh288diesh@gmail.com\n',
-                  'Danika\n', 'DESH288DIESH@GMAIL.COM\n', 'DESH288DIESH@GMAIL.ccc\n'])
-
-print('----------Last Max Word------------')
+    f.writelines(['line2\n', '\n', 'line3\n',
+                  'line444444 444\n', 'line555555 555\n', 'line6\n'])
+print('--------Result------------')
+max_val = ''
 for item in open('hw#20.txt', 'r'):
-    list_words = list(item)
-    max_value = list_words[0]
-    if len(item) >= len(max_value):
-        max_value = item
-print(f'The last string in the file with the maximum lengthh: {max_value}'
-      f'String lenght: {len(max_value)} symbols\n'
-      f'Type: {type(max_value)}')
-
+    if len(item) >= len(max_val):
+        max_val = item
+print(f'The last word in the file with the maximum lengthh: {max_val}')
 
 
