a32aabbaa91e78fd82a3fd2aa0c2663130918bb6
desh288ddiesh <deshdesh288@gmail.com>
2024-05-08 23:25:28 +0300

    HW#22

--- lecture#7/hw#22.py
+++ lecture#7/hw#22.py
@@ -2,12 +2,15 @@ import json
 
 # lst2dict = ([0, 1, 2, 3])
 # lst2dict = (['a', 'A', 'b', 'B', 'c'])
-lst2dict = (['a', None, 'C'])
+# lst2dict = (['a', None, 'C'])
 # lst2dict = ([])
+lst2dict = None
 
 
 def list2dict(lst):
-    if len(lst) % 2 != 0:
+    if lst is None:
+        result = None
+    elif len(lst) % 2 != 0:
         lst.pop()
         result = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
     elif len(lst) % 2 == 0:
