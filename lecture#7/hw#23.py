d9cc76bad19ec044d1bcf518344bb8e6bb26594e
desh288ddiesh <deshdesh288@gmail.com>
2024-05-09 14:21:24 +0300

    HW#23

--- lecture#7/hw#23.py
+++ lecture#7/hw#23.py
@@ -1,13 +1,15 @@
 # second_largest_number = ([])  # None
 # second_largest_number = ([1, 1])  # None
-second_largest_number = ([1, 2, 3, 4, 5])  # 4
+# second_largest_number = ([1, 2, 3, 4, 5])  # 4
+# second_largest_number = ([-1, -2, -3, -4, -5])
+second_largest_number = ([0, 0, 0, 0, 0, 0])
 
 
 def second_largest_number_(lst):
     max_number = 0
     second_max_number = 0
     for i in lst:
-        if i > max_number:
+        if i > max_number or i < max_number:
             second_max_number = max_number
             max_number = i
         elif i > second_max_number and i != max_number:
