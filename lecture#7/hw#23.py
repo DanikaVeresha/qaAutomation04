39c1df954492f762cc65763cc39ef8738cf154e2
desh288ddiesh <deshdesh288@gmail.com>
2024-05-09 05:07:55 +0300

    HW#23#24

---
+++ lecture#7/hw#23.py
@@ -0,0 +1,20 @@
+# second_largest_number = ([])  # None
+# second_largest_number = ([1, 1])  # None
+second_largest_number = ([1, 2, 3, 4, 5])  # 4
+
+
+def second_largest_number_(lst):
+    max_number = 0
+    second_max_number = 0
+    for i in lst:
+        if i > max_number:
+            second_max_number = max_number
+            max_number = i
+        elif i > second_max_number and i != max_number:
+            second_max_number = i
+    if second_max_number == 0 or len(lst) == 0:
+        return None
+    return second_max_number
+
+
+print(second_largest_number_(second_largest_number))
---
+++ lecture#7/hw#24.py
@@ -0,0 +1,4 @@
+dates = ['14-Dec', '12-Apr', '13-Apr', '31-Dec', '1-Jan', '12-Jan']
+dates.sort(key=lambda x: int(x.split('-')[0] if x.split('-')[1] != 'Jan' else 0))
+print(dates)
+
