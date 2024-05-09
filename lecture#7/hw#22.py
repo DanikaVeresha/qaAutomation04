7bcc1a7084f22835a3f7ee477ed0244d6f11796d
desh288ddiesh <deshdesh288@gmail.com>
2024-05-09 03:33:12 +0300

    HW#22

--- lecture#7/hw#22.py
+++ lecture#7/hw#22.py
@@ -1,30 +1,15 @@
-import json
-lst2dict = ([0, 1, 2, 3])
-# lst2dict = (['a', 'A', 'b', 'B', 'c'])
+
+# lst2dict = ([0, 1, 2, 3])
+lst2dict = (['a', 'A', 'b', 'B', 'c'])
 # lst2dict = (['a', None, 'C'])
 # lst2dict = ([])
-# lst2dict = None
 
 
-# def list2dict(lst):
-#     if len(lst) % 2 != 0:
-#         lst.pop()
-#         result = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
-#     else:
-#         result = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
-#     return result
-
-def list2dict(lst):
+def list2dict_(lst):
     if len(lst) % 2 != 0:
         lst.pop()
-        result = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
-    else:
-        return None
-    return result
+    return {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
 
 
-print(f'List: {lst2dict}; Type: {type(lst2dict)}')
-print(f'Result: {list2dict(lst2dict)}; Type: {type(list2dict(lst2dict))}')
-
-
+print(list2dict_(lst2dict))
 
