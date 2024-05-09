6003b98dc00a152d292531f6b4a80918ab48db0b
desh288ddiesh <deshdesh288@gmail.com>
2024-05-09 20:38:45 +0300

    HW#24

--- lecture#7/hw#24.py
+++ lecture#7/hw#24.py
@@ -3,12 +3,13 @@ dates = ['1-Dec', '12-Apr', '17-Apr', '31-Dec', '19-Jan', '4-Jan', '12-Dec']
 
 
 def sortdates(dates):
-    month_order = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    month = month_order.index(dates.split('-')[1])
+    month_ = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    month = month_.index(dates.split('-')[1])
     day = int(dates.split('-')[0])
     return month, day
 
 
 dates.sort(key=sortdates)
 print(dates)
+
 
