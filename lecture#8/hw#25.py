b6e705182c4c1e3e20dda837f104ee6e11e8353d
desh288ddiesh <deshdesh288@gmail.com>
2024-05-10 20:50:46 +0300

    HW#25

--- lecture#8/hw#25.py
+++ lecture#8/hw#25.py
@@ -1,8 +1,9 @@
 
+
 def skip_if(condition, reason=''):
     def decorator(func):
         def wrapper(*args, **kwargs):
-            if condition == True:
+            if condition:
                 print(f'Skipping test "{func.__name__}". Reason: {reason}')
             else:
                 func(*args, **kwargs)
@@ -10,14 +11,15 @@ def skip_if(condition, reason=''):
     return decorator
 
 
-a = 2
-b = 4
-result = sum([a, b]) == 5
+@skip_if(True, 'This test is skipped')
+def test_sum():
+    assert sum([1, 2, 3]) == 6
 
 
-@skip_if(result, f'Condition met. We got {result}, because {a} + {b} = {a + b}')
-def func_():
-    assert result, f'Expected 5, but got {result} because {a} + {b} = {a + b}'
+@skip_if(False)
+def test_sum_():
+    assert sum([2, 2, 3]) == 6, 'Expected 6'
 
 
-func_()
+test_sum()
+test_sum_()
