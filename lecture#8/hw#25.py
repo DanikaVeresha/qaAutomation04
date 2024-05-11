eb7645876abc4f68ae67fa04c6d1d8cf190f5ca0
desh288ddiesh <deshdesh288@gmail.com>
2024-05-11 17:01:42 +0300

    hw#25

--- lecture#8/hw#25.py
+++ lecture#8/hw#25.py
@@ -5,23 +5,22 @@ def skip_if(condition, reason=''):
             if condition:
                 print(f'Skipping test "{func.__name__}". Reason: {reason}')
             else:
-                func(*args, **kwargs)
+                return func(*args, **kwargs)
         return wrapper
     return decorator
 
 
-def test_skip_if(*args, **kwargs):
-    num = 25
-    result = sum(args, **kwargs) == num
-    @skip_if(result, f'becouse sum entered values is equal to {num} and so condition is met.')
-    def test_func(*args, **kwargs):
-        assert result, (f'The expected sum of the values entered should be {num}, but we '
-                        f'got {sum(args, **kwargs)}')
-
-    test_func(*args, **kwargs)
+values = [10, 10, 5]
+if_ = 25
 
 
-test_skip_if(5, 10, 10)
+@skip_if(condition=sum(values) == if_,
+         reason=f'becouse sum entered values is equal to {sum(values)} and so condition is met.')
+def test_func(values):
+    return sum(values)
+
+
+print(test_func(values))
 
 
 
