3bd67bec69d1719e153d1917e84093637c78f7fb
desh288ddiesh <deshdesh288@gmail.com>
2024-05-21 18:30:35 +0300

    hw#31

--- lecture#9/hw#31.py
+++ lecture#9/hw#31.py
@@ -1,5 +1,5 @@
 """Task 31. Please look at the wiki_page.txt file because I have changed it."""
-import re
+# import re
 
 
 def get_links(html_file):
@@ -7,16 +7,32 @@ def get_links(html_file):
     with open(html_file, 'r') as file:
         for line in file:
             value = line.find('<a')
-            value_end = line.rfind('</a>')
+            value_end = line.find('</a>')
+            value_ends = line.rfind('<a')
+            value_end_ends = line.rfind('</a>')
             line_values = line[value:value_end]
-            result = re.findall(r'href="([^"]*)"', line_values)
-            for res_line in result:
-                yield res_line
+            # print(line_values)
+            line_values_end = line[value_ends:value_end_ends]
+            # print(line_values_end)
+            if line_values == line_values_end:
+                if 'href="' in line_values:
+                    start = line_values.find('href="') + len('href="')
+                    end = line_values.find('"', start)
+                    yield line_values[start:end]
+            else:
+                if 'href="' in line_values and 'href="' in line_values_end:
+                    start = line_values.find('href="') + len('href="') and line_values_end.find('href="') + len('href="')
+                    end = line_values.find('"', start) and line_values_end.find('"', start)
+                    yield line_values[start:end], line_values_end[start:end]
 
 
 for item in get_links('wiki_page.txt'):
-    print(item)
-
+    if type(item) is tuple:
+        for i in item:
+            print(i)
+    else:
+        print(item)
+# get_links('wiki_page.txt')
 
 
 
