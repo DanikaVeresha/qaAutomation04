1976a6ff6f31ca156a3b587bd76529091c0c2091
desh288ddiesh <deshdesh288@gmail.com>
2024-05-21 04:20:05 +0300

    hw#32

---
+++ lecture#10/hw#32.py
@@ -0,0 +1,20 @@
+"""Task 32. Random string"""
+import random
+
+
+def random_string(length):
+    """Generates a random string of the specified length"""
+    res = (
+        "".join(chr(random.randint(48, 57)) for _ in range(length)) +
+        "".join(chr(random.randint(65, 90)) for _ in range(length)) +
+        "".join(chr(random.randint(97, 122)) for _ in range(length))
+
+    )
+    return "".join(random.sample(res, length))
+
+
+print(f'Random string: {random_string(10)}')
+# print(type(random_string(10)))
+
+
+
