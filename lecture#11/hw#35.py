252bc404285a0f17d41c2b9bb5128eb238b057dc
desh288ddiesh <deshdesh288@gmail.com>
2024-05-27 22:50:20 +0300

    hw#35

--- lecture#11/hw#35.py
+++ lecture#11/hw#35.py
@@ -2,6 +2,7 @@
 Task 35. Classes creation
 """
 import datetime
+from typing import Union
 
 
 class BankAccount:
@@ -11,47 +12,55 @@ class BankAccount:
     date = datetime.datetime.utcnow()
     inflation_percentage = 7.5
 
-    def __init__(self, name, balance=1000000):
+    def __init__(self, name, balance=1000):
         """Initializes the BankAccount class with the name and balance of the account holder.
         The default balance is 1000000."""
         self.name = name
         self.balance = balance
 
-    def deposit(self, amount):
+    def add_money_on_account(self, amount):
         """Adds the amount to the balance of the account holder."""
         self.balance += amount
-        return f'Operation “Replenishment account” of the account owner\n' \
-               f'Account holder: {self.name}\nBalance: {self.balance}$\nDate of operation: {self.date}'
 
-    def withdraw(self, amount):
+    def add_money_account_info(self, amouth):
+        """Returns information about the account holder's balance after adding money to the account."""
+        self.add_money_on_account(amouth)
+        return f'Operation "Add money on account" of the account owner\n' \
+               f'Account holder: {self.name}\nBalance: {self.balance}$\nDate of operation: {self.date}\n'
+
+    def withdraw_money_from_account(self, amount):
         """Subtracts the amount from the balance of the account holder."""
-        if amount > self.balance:
-            return 'Insufficient funds for transaction!'
         self.balance -= amount
-        return f'Operation "Withdraw money" of the account owner\n' \
-               f'Account holder: {self.name}\nBalance: {self.balance}$\nDate of operation: {self.date}'
+
+    def withdraw_money_account_info(self, amount):
+        """Returns information about the account holder's balance after withdrawing money from the account."""
+        self.withdraw_money_from_account(amount)
+        if self.balance < 0:
+            return 'Insufficient funds for transaction!\n'
+        return f'Operation "Withdraw money from account" of the account owner\n' \
+               f'Account holder: {self.name}\nBalance: {self.balance}$\nDate of operation: {self.date}\n'
 
     @staticmethod
-    def annual_percentage(balance, percentage):
+    def annual_percentage(balance, percentage: Union[int, float]):
         """Calculates the annual percentage of the account holder's balance."""
         return f'Account balance: {balance}$\nAnnual percentage {percentage}%\n' \
                f'Income: {(balance /100) * percentage}$\n' \
                f'Balance after annual percentage: {balance + (balance / 100) * percentage}$\n'
 
     @classmethod
-    def inflation(cls, new_inflation_percentage, percentage, balance):
+    def inflation(cls, new_inflation_percentage: Union[int, float], percentage: Union[int, float], balance):
         """Calculates the inflation of the account holder's balance at the end of the year"""
         cls.inflation_percentage = new_inflation_percentage
         if new_inflation_percentage > percentage:
             result_losses = ((percentage - new_inflation_percentage) / 100) * balance
-            return f'If the inflation rate has increased during the current year since your deposit, then:\n' \
+            return f'Inflation rate has increased during the current year since your deposit, result be:\n' \
                    f'At the end of the year your losses: {round(result_losses, 2)}$\nYour start balance: {balance}$\n'\
                    f'Balance after inflation: {balance + result_losses}$\n'
         else:
             result_income = ((percentage - new_inflation_percentage) / 100) * balance
-            return f'If the inflation rate at the time of expiration of 365 days from the date\n' \
+            return f'Inflation rate at the time of expiration of 365 days from the date\n' \
                    f'of your deposit is lower than the inflation rate at the time you opened a \n' \
-                   f'deposit account with our bank, then:\n' \
+                   f'deposit account with our bank, result be:\n' \
                    f'Your income at the end of the year: {round(result_income, 2)}$\nYour start balance: {balance}$\n' \
                    f'Balance after inflation: {balance + result_income}$\n'
 
@@ -59,7 +68,7 @@ class BankAccount:
         """Returns the account owner's name, balance, and account opening date.
         String representation of an object, focusing on readability"""
         return f'Account holder: {self.name}\nBalance: {self.balance}$\n' \
-               f'Account opening date: {self.date}'
+               f'Account opening date: {self.date}\n'
 
 
 class BTCAccount(BankAccount):
@@ -76,41 +85,50 @@ class BTCAccount(BankAccount):
         super().__init__(name, balance)
         self.btc = btc
 
-    def buy_btc(self, btc_price):
+    def buy_btc(self, btc_price: Union[int, float]):
         """The method calculates the cost of buying bitcoins and subtracts the amount from
         the balance of the account holder."""
         cost = self.btc * btc_price
-        if cost > self.balance:
-            return 'Insufficient funds for transaction!'
         self.balance -= cost
+
+    def buy_btc_info(self, btc_price: Union[int, float]):
+        """Returns information about the account holder's balance after buying bitcoins."""
+        self.buy_btc(btc_price)
+        if self.balance < 0:
+            return 'Insufficient funds for transaction!\n'
         return f'Operation “Buy BTC” of the account owner\n' \
                f'Account holder: {self.name}\nNumber of bitcoins purchased: {self.btc}\n' \
-               f'Price of one BTC: {btc_price}\nYour balance after purchase BTC: {self.balance}$\n' \
-               f'Date of operation: {self.date}'
+               f'Price of one BTC: {btc_price}$\nYour balance after purchase BTC: {self.balance}$\n' \
+               f'Date of operation: {self.date}\n'
 
-    def sell_btc(self, btc_price):
+    def sell_btc(self, btc_price: Union[int, float]):
         """The method calculates the cost of selling bitcoins and adds the amount to the
         balance of the account holder."""
         cost = self.btc * btc_price
         self.balance += cost
+
+    def sell_btc_info(self, btc_price: Union[int, float]):
+        """Returns information about the account holder's balance after selling bitcoins."""
+        self.sell_btc(btc_price)
         return f'Operation “Sell BTC” of the account owner\n' \
                f'Account holder: {self.name}\nNumber of bitcoins for sale: {self.btc}\n' \
-               f'Price of one BTC: {btc_price}\nYour balance after sale BTC: {self.balance}$\n' \
-               f'Date of operation: {self.date}'
+               f'Price of one BTC: {btc_price}$\nYour balance after sale BTC: {self.balance}$\n' \
+               f'Date of operation: {self.date}\n'
 
     @staticmethod
     def btc_delta(bid, ask):
         """Bitcoin cumulative delta indicator for the one day"""
         cum_delta = bid - ask
         if cum_delta > 0:
-            return f'Bitcoin cumulative delta indicator for the month V: +{cum_delta}\n' \
-                   f'The market is dominated by buyers'
+            return f'Bitcoin cumulative delta indicator for the month V: +{round(cum_delta, 4)}\n' \
+                   f'The market is dominated by buyers\n'
         else:
-            return f'Bitcoin cumulative delta indicator for the month: {cum_delta}\n' \
-                   f'The market is dominated by sellers'
+            return f'Bitcoin cumulative delta indicator for the month: {round(cum_delta, 4)}\n' \
+                   f'The market is dominated by sellers\n'
 
     @classmethod
-    def btc_change(cls, new_btc_exchange_rate_buy, new_btc_exchange_rate_sell, total_btc):
+    def btc_change(cls, new_btc_exchange_rate_buy: Union[int, float],
+                   new_btc_exchange_rate_sell: Union[int, float], total_btc: int):
         """The method calculates the cost of buying and selling bitcoins and returns the difference
         at the changed bitcoin rate"""
         print(f'Old price BTC buy: {cls.btc_exchange_rate_buy}$ - New price BTC: {new_btc_exchange_rate_buy}$')
@@ -120,42 +138,31 @@ class BTCAccount(BankAccount):
         cls.btc_exchange_rate_sell = new_btc_exchange_rate_sell
         cost = total_btc * new_btc_exchange_rate_buy
         income = total_btc * new_btc_exchange_rate_sell
-        return f'Cost of buying {total_btc} bitcoins at the new price: {cost}$\n' \
-               f'Income from selling {total_btc} bitcoins at the new price: {income}$\n'
+        return f'Cost of buying {total_btc} bitcoins at the new price: {round(cost, 4)}$\n' \
+               f'Income from selling {total_btc} bitcoins at the new price: {round(income, 4)}$\n'
 
     def __str__(self):
         """Returns the account owner's name, balance, number of bitcoins, and account opening date.
         String representation of an object, focusing on readability"""
         return f'Account holder: {self.name}\nBalance: {self.balance}$\n' \
-               f'Number of bitcoins: {self.btc}\nAccount opening date: {self.date}'
+               f'Number of bitcoins: {self.btc}\nAccount opening date: {self.date}\n'
 
 
-print('-------------------------Results Jane Doe-------------------------')
-account = BankAccount('Jane Doe')
-print(str(account))
-print()
-print(str(account.deposit(1000)))
-print()
-print(str(account.withdraw(500)))
-print()
-print(str(BankAccount.annual_percentage(100000, 6.5)))
-print(str(account.annual_percentage(100000, 6.5)))
-print()
-print(str(BankAccount.inflation(13, 6.5, 100000)))
-print('-------------------------Results Nik Smith-------------------------')
-btc_account = BTCAccount(5, 'Nik Smith', 1000000)
-print(str(btc_account))
-print()
-print(str(btc_account.buy_btc(69074.44)))
-print()
-print(str(btc_account.sell_btc(69089.79)))
-print()
-print(str(BTCAccount.btc_delta(1300000, 1700060)))
-print(str(btc_account.btc_delta(1300000, 1700060)))
-print()
-print(str(BTCAccount.btc_change(69079.45, 69099.45, 2)))
-print()
-
-
+print('-------------------------Results BankAccount-------------------------')
+user_bank = BankAccount('Jane Doe')
+print(user_bank)
+print(user_bank.add_money_account_info(500))
+print(user_bank.withdraw_money_account_info(100))
+print(user_bank.annual_percentage(1000, 7.5))
+print(BankAccount.inflation(6.5, 7.5,
+                            1000))
+print('-------------------------Results BTCAccount-------------------------')
+user = BTCAccount(21, 'John Doe')
+print(user)
+print(user.buy_btc_info(69074.44))
+print(user.sell_btc_info(69089.79))
+print(user.btc_delta(10734, 17564))
+print(BTCAccount.btc_change(69075.44,
+                            69090.79, 5))
 
 
