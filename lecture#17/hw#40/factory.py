ed6b2ca11c4b19fe4c4ae77bec4022b164380e9b
daria <deshdesh288@gmail.com>
2024-06-14 23:10:41 +0300

    hw#40

---
+++ lecture#17/hw#40/factory.py
@@ -0,0 +1,101 @@
+from abc import ABC, abstractmethod
+from airplane import Airplane
+from car import Car
+from train import Train
+
+
+class AbstractCreatingRoute(ABC):
+    @abstractmethod
+    def create_route(self, message, start_point, end_point):
+        """Create route for a vehicle"""
+        pass
+
+
+class AirplaneFactory(AbstractCreatingRoute):
+    def __init__(self):
+        self.mapping = {
+            'AirLine': Airplane,
+            'AmericanAirlines': Airplane,
+            'UnitedAirlines': Airplane,
+            'DeltaAirlines': Airplane,
+        }
+
+    def create_route(self, message, start_point, end_point):
+        """Create route for an airplane. Distance can to change because of random speed."""
+        return self.mapping.get(message, Airplane)(start_point, end_point).the_plane_took_off(), message
+
+
+class CarFactory(AbstractCreatingRoute):
+    def __init__(self):
+        self.mapping = {
+            'Toyota': Car,
+            'Ford': Car,
+            'Chevrolet': Car,
+            'Audi': Car,
+            'BMW': Car,
+            'Mercedes': Car,
+            'Tesla': Car,
+            'Hyundai': Car,
+            'Kia': Car,
+            'Nissan': Car,
+            'Honda': Car,
+            'Mazda': Car,
+            'Subaru': Car,
+            'Volkswagen': Car,
+            'Jeep': Car
+        }
+
+    def create_route(self, message, start_point, end_point):
+        """Create route for a car. Distance can to change because of random speed."""
+        return self.mapping.get(message, Car)(start_point, end_point).the_car_started(), message
+
+
+class TrainFactory(AbstractCreatingRoute):
+
+    def __init__(self):
+        self.mapping = {
+            'Amtrak': Train,
+            'UnionPacific': Train,
+            'BNSF': Train,
+            'NorfolkSouthern': Train,
+            'CSX': Train,
+            'CanadianNational': Train,
+            'CanadianPacific': Train,
+            'KansasCitySouthern': Train,
+            'Ferromex': Train,
+            'FloridaEastCoast': Train
+        }
+
+    def create_route(self, message, start_point, end_point):
+        """Create route for a train. Distance can to change because of random speed."""
+        return self.mapping.get(message, Train)(start_point, end_point).the_train_departed(), message
+
+
+class FactoryProducer:
+    def __init__(self):
+        self.mapping = {
+            'airplane': AirplaneFactory,
+            'car': CarFactory,
+            'train': TrainFactory,
+        }
+
+    def get_factory(self, message):
+        """Get factory"""
+        return self.mapping.get(message, CarFactory)()
+
+
+factory_producer = FactoryProducer()
+factory = factory_producer.get_factory('car')
+print(factory.create_route('Audi', 'Kyiv', 'Dnipropetrovsk'))
+print(factory.create_route('car', 'Kyiv', 'Dnipropetrovsk'))
+print('----------------------------------------------------------------------------------')
+factory = factory_producer.get_factory('train')
+print(factory.create_route('Amtrak', 'New York', 'Los Angeles'))
+print(factory.create_route('train', 'New York', 'Los Angeles'))
+print('----------------------------------------------------------------------------------')
+factory = factory_producer.get_factory('airplane')
+print(factory.create_route('AmericanAirlines', 'Kyiv', 'New York'))
+print(factory.create_route('airplane', 'Kyiv', 'New York'))
+print('----------------------------------------------------------------------------------')
+factory = factory_producer.get_factory('diesh')
+print(factory.create_route('diesh', 'Kyiv', 'New York'))
---
+++ lecture#17/hw#40/train.py
@@ -0,0 +1,57 @@
+"""Calculate the distance between points for a train on the ground"""
+
+import datetime
+import random
+from geopy.geocoders import Nominatim
+from haversine import haversine, Unit
+
+
+class Train:
+
+    def __init__(self, start_point, end_point):
+        self.start_point = start_point
+        self.end_point = end_point
+
+    def get_coordinates(self):
+        """Get coordinates for start and end points."""
+        geolocator = Nominatim(user_agent="train_app")
+        location_start = geolocator.geocode(self.start_point)
+        location_end = geolocator.geocode(self.end_point)
+        return location_start, location_end
+
+    def create_route(self):
+        """Create route for a train."""
+        start_point, end_point = self.get_coordinates()
+        start_point_address = start_point.address
+        end_point_address = end_point.address
+        return f'{start_point_address} -> {end_point_address}'
+
+    def calculate_distance(self):
+        """Calculate the distance between points for a train on the ground.
+        Distance can to change because of random speed."""
+        start_point, end_point = self.get_coordinates()
+        start_point_coordinates = (start_point.latitude, start_point.longitude)
+        end_point_coordinates = (end_point.latitude, end_point.longitude)
+        distance = haversine(start_point_coordinates, end_point_coordinates, unit=Unit.KILOMETERS)
+        return round(distance, 2)
+
+    @staticmethod
+    def calculate_speed():
+        """Calculate speed for a train."""
+        speed = random.randint(140, 200)
+        return speed
+
+    def calculate_time(self):
+        """Calculate time for a train to drive from start to end points."""
+        time = self.calculate_distance() / self.calculate_speed()
+        time = datetime.timedelta(hours=time)
+        return time
+
+    def the_train_departed(self):
+        """Return information about the train."""
+        return f'Route: {self.create_route()}\n' \
+               f'Distance: {self.calculate_distance()} km\n' \
+               f'Speed: {self.calculate_speed()} km/h\n' \
+               f'Travel duration: {self.calculate_time()}\n'
+
+
