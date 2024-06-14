61a01a9aa11f49a784b3e3c2d1e661627e3eb46e
daria <deshdesh288@gmail.com>
2024-06-14 23:55:37 +0300

    hw#2->hw#40

---
+++ lecture#2/hw#4.py
@@ -0,0 +1,8 @@
+"""Task 4."""
+
+x = int(input('Enter a: '))
+y = int(input('Enter b: '))
+res_sum = x + y
+res_product = x * y
+print(f'{x} + {y} = {res_sum}')
+print(f'{x} + {y} = {res_product}')
---
+++ lecture#6/hw#20.py
@@ -0,0 +1,13 @@
+"""Task 20."""
+
+with open('hw#20.txt', 'w') as f:
+    f.writelines(['line2\n', '\n', 'line3\n',
+                  'line444444 444\n', 'line555555 555\n', 'line6\n'])
+print('--------Result------------')
+max_val = ''
+for item in open('hw#20.txt', 'r'):
+    if len(item) >= len(max_val):
+        max_val = item
+print(f'The last word in the file with the maximum lengthh: {max_val}')
+
+
