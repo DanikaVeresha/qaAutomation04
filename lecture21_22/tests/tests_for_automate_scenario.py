7d53352b201e9e0b6c890aa333fe7257e6de94cc
daria <deshdesh288@gmail.com>
2024-07-05 21:30:55 +0300

    TASK -> 21 - 22

--- lecture21_22/tests/tests_for_automate_scenario.py
+++ lecture21_22/tests/tests_for_automate_scenario.py
@@ -311,3 +311,4 @@ def test_N_if_enter_invalid_username_password(username, password):
     timer(lambda: login_button.click(), 4)
     timer(lambda: driver.save_screenshot('result_of_confirmed_login_for_invalid_danika.png'), 2)
 
+
