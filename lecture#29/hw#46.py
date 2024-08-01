ace3a401be7d64144a046726a3646c852a24ebf0
daria <deshdesh288@gmail.com>
2024-08-01 13:45:36 +0300

    HomeWork#46

--- lecture#29/hw#46.py
+++ lecture#29/hw#46.py
@@ -2,19 +2,12 @@ import os
 import random
 import string
 import datetime
+import threading
 import time
 from threading import Thread
 
 
 def file_generator(directory, number_of_files, size):
-    """
-    1. Write a function that generates `number_of_files` files in the `directory` directory.
-    The contents of the files must be random and consist of large/small Latin letters, numbers and
-    punctuation symbols.
-    The file must contain a random number of characters ranging from `size/2` to `size` characters.
-    def file_generator(directory, number_of_files, size)
-    Be careful: file_generator('files', 200, 1000_000) ~150 Mb
-    """
     if not os.path.exists(directory):
         os.makedirs(directory)
 
@@ -26,79 +19,110 @@ def file_generator(directory, number_of_files, size):
 
 
 def letter_counter_in_one_thread(directory, letter_to_find):
-    """
-    2. Write a function (usual, that runs in single thread) that returns the number of letters
-    `letter_to_find` in all files in the directory `directory`
-    def letter_counter_in_one_thread(directory, letter_to_find)
-    """
     count_letters = 0
     files = os.listdir(directory)
     for file in files:
         with open(f'{directory}/{file}') as f:
             count_letters += f.read().count(letter_to_find)
 
-    time.sleep(0.1)
-    print(f'--> Result "Thread" -> {count_letters} times')
     return count_letters
 
 
 def letter_counter_in_n_threads(directory, letter_to_find, number_of_threads):
-    """
-    3. Write a function that returns the number of letters `letter_to_find` in all files in the
-    directory `directory`
-    The function should split the files in the directory into `number_of_threads` groups, and
-    read/count the letters for each group in a separate thread.
-    Groups should be split equally as it is possible.
-    def letter_counter_in_n_threads(directory, letter_to_find, number_of_threads)
-    """
     files = os.listdir(directory)
     groups = [files[i::number_of_threads] for i in range(number_of_threads)]
-    counter = 0
+    results = []
+    lock = threading.Lock() # Создаю объект блокировки
+
+    def count_letters(groups): # Функция для подсчета букв
+        count = 0
+        for file in group:
+            with open(f'{directory}/{file}') as f:
+                count += f.read().count(letter_to_find)
+        with lock: # Блокирую доступ к общему ресурсу
+            results.append(count)
+
+    # Создаю потоки
+    threads = []
     for group in groups:
-
-        def letter_counter(group):
-            nonlocal counter
-            count_letters = 0
-            for file in group:
-                with open(f'{directory}/{file}') as f:
-                    count_letters += f.read().count(letter_to_find)
-            counter += count_letters
-
-            time.sleep(0.5)
-            print(f'Result "Threads" -> {count_letters} times')
-            return count_letters
-
-        thread = Thread(target=letter_counter, args=(group,))
+        thread = Thread(target=count_letters, args=(group,))
         thread.start()
-        thread.join(timeout=0.2)
+        threads.append(thread)
 
-    print(f'--> Total count result "Threads" -> {counter} times')
-    return counter
+    # Жду завершения всех потоков
+    for thread in threads:
+        thread.join()
+
+    return results
 
 
-def main():
-    print(f"{file_generator('files', 12, 100)} files were created\n"
-          f"------------------------------------------------")
+def client_code(directory, number_of_files, size, letter_to_find, number_of_threads):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-    time2 = datetime.datetime.now()
-    letter_counter_in_n_threads('files', 'A', 6)
-    t2 = datetime.datetime.now() - time2
+    for i in range(number_of_files):
+        with open(f'{directory}/file_{i}.txt', 'w') as file:
+            file.write(''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=random.randint(size // 2, size))))
 
-    time1 = datetime.datetime.now()
-    thread1 = Thread(target=letter_counter_in_one_thread, args=('files', 'A'))
-    thread1.start()
-    thread1.join()
-    t1 = datetime.datetime.now() - time1
+    counter = 0
+    files = os.listdir(directory)
+    start3 = datetime.datetime.now()
+    for file in files:
+        with open(f'{directory}/{file}') as f:
+            counter += f.read().count(letter_to_find)
+    time3 = datetime.datetime.now() - start3
 
-    print(f'\nLead time "Thread" -> {t1}\nLead time "Threads" -> {t2}')
+    groups = [files[i::number_of_threads] for i in range(number_of_threads)]
+    results_threads = []
+    lock = threading.Lock()
 
-    if t1 > t2:
-        print(f'Threads is faster than Thread on -> {t1 - t2}\n')
+    def count_letters(groups):
+        count = 0
+        for file in group:
+            with open(f'{directory}/{file}') as f:
+                count += f.read().count(letter_to_find)
+        with lock:
+            results_threads.append(count)
+
+    threads = []
+    start4 = datetime.datetime.now()
+    for group in groups:
+        thread = Thread(target=count_letters, args=(group,))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+    time4 = datetime.datetime.now() - start4
+
+    total_count_letters = sum(results_threads)
+
+    if time3 > time4:
+        print(f'-> The "Threads" method is faster on {time3 - time4}')
     else:
-        print(f'Thread is faster than Threads on -> {t2 - t1}\n')
+        print(f'-> The "Thread" method is faster on {time4 - time3}')
+
+    return f'{number_of_files} | {counter} -> {time3} | {total_count_letters} -> {time4} |'
 
 
 if __name__ == '__main__':
-    main()
+    print(f'-> Number of files created: '
+          f'{file_generator("files", 12, 50)}\n')
+
+    start1 = datetime.datetime.now()
+    print(f'-> Result "Thread": '
+          f'{letter_counter_in_one_thread("files", "A")} times')
+    print(f'-> Lead time "Thread": {datetime.datetime.now() - start1}\n')
+
+    start2 = datetime.datetime.now()
+    print(f'-> Result "Threads": '
+          f'{letter_counter_in_n_threads("files", "A", 6)} times')
+    print(f'-> Lead time "Threads": {datetime.datetime.now() - start2}\n')
+
+    print(f'-> Result Client Code: \n'
+          f'\tNumber of files | Number of letters -> Time "Thread" | Number of letters -> Time "Threads" | \n'
+          f'\t{client_code("files", 12, 50, "A", 4)}')
+
+
 
 
